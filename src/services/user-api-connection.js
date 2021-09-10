6fac84abdbfdf35b278626f61360758474128ee3
jeremias-2000 <jdossantospinheiro20@gmail.com>
2021-09-09 21:35:32 -0300

    mudar rota de conexão local para à do heroku

--- src/services/user-api-connection.js
+++ src/services/user-api-connection.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const connection = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/'
+    baseURL: 'https://book-deploy.herokuapp.com/api/v1'
 })
 
 export default connection;
